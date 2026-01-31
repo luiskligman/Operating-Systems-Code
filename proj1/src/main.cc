26fc20d70860e0012237a61d45a0596d736df13d
Luis Kligman <137526222+luiskligman@users.noreply.github.com>
2026-01-31 18:09:13 -0500

    Add files via upload

--- proj1/src/main.cc
+++ proj1/src/main.cc
@@ -8,18 +8,45 @@
 #include <pthread.h>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 // Portable Operating System Interface (POSIX)
 #include <unistd.h>
 
+static int max_threads;
+static volatile int release_threads;
+
+// struct to store the input from the file
 struct Task {
     std::string id;
     std::string name;
     int amount;
 };
 
+// struct to properly store info relating to each thread
+// struct allows user to quickly add fields / remove fields
+struct thread_info {
+    int id;
+};
+
 // the work that each thread will perform
 void *worker(void *arg) {
+    thread_info* info = static_cast<thread_info*>(arg);
+
+    // too many threads spun compared to users max threads value
+    if (max_threads < info->id) {
+        printf("id greater than max_howthreads: %d\n", info->id);
+        return NULL;
+    }
+
+    // threads in thread pool must sleep until specific
+    while (release_threads == 0) {
+        Timings_SleepMs(1000);
+        printf("slept");
+    }
+
+    printf("id: %d\n", info->id);
+
     return NULL;
 }
 
@@ -27,6 +54,9 @@ int main(int argc, char *argv[]) {
     // capture the number of tasks which is the first row in the piped input file
     int n;
     std::cin >> n;
+
+    // init release_threads to 0 so that the threads will sit idle in the thread pool
+    release_threads = 0;
 
     // create a vector of size n (n is known)
     std::vector<Task> tasks(n);
@@ -38,16 +68,42 @@ int main(int argc, char *argv[]) {
     // (via dev/tty) for a number, k, of threads to use for this execution
     std::cerr << "enter how many k threads you wish to use for this execution\n";
     std::ifstream tty_in("/dev/tty");
-    if (tty_in) {
-        int var;
-        tty_in >> var;
-    }
+    // if (tty_in) {
+    //     int var;
+    //     tty_in >> var;
+    // }
+    // if (tty_in) {
+    //     tty_in >> max_threads;
+    // }
+    max_threads = 5;
 
     // figure out how many online threads the host computer currently has
     long online_threads = sysconf(_SC_NPROCESSORS_ONLN);
-
     printf("online threads: %ld\n", online_threads);
 
+    // create two separate vectors the size of the number of online_threads
+    // keep track of threads and accompanying info
+    std::vector<pthread_t> threads(online_threads);
+    std::vector<thread_info> info(online_threads);
+
+    // spin up threads
+    for (int i = 0; i < online_threads; i++) {
+        info[i].id = i + 1;  // threads are indexed starting at 1
+        pthread_create(&threads[i], NULL, worker, &info[i]);
+    }
+
+
+    //
+    // pthread_t p1;
+    // thread_info info;
+    // info.id = 1;
+    // pthread_create(&p1, NULL, worker, &info);
+
+
+    // make sure all threads are complete (using join)
+    for (int i = 0; i < online_threads; i++) {
+        pthread_join(threads[i], NULL);  // waits for the thread to complete
+    }
 
 
 
